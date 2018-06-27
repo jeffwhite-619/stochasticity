b1d3aa44417044b14a000bc79440d320f5ffb6b7
Jeff White <thejeffwhite@gmail.com>
2018-06-27 10:33:41 -0700

    Moved coin to its own module, added a simple test, new .gitignore file

--- coin/test/test_coin.py
+++ coin/test/test_coin.py
@@ -1,13 +1,19 @@
+import unittest
 import random
+from stochasticity.coin.coin import Coin
 
 
-class Coin:
-    @staticmethod
-    def flip(flips: int = 0):
-        if flips < 1:
-            flips = random.randint(1, 50)
-        coinflip = random.choice([True, False])
-        if flips > 1:
-            for i in range(flips - 1):
-                coinflip = random.choice([True, False])
-        return coinflip
+class TestCoin(unittest.TestCase):
+    _values = [True, False]
+
+    def test_flip(self):
+        test_flip = Coin.flip()
+        self.assertIn(test_flip, self._values)
+        flips = random.randrange(1, 500)
+        test_flip = Coin.flip(flips)
+        self.assertIn(test_flip, self._values)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
