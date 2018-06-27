4c35188724b232d0a828eafe1c0fc5fdd04cffca
Jeff White <thejeffwhite@gmail.com>
2018-06-26 19:23:06 -0700

    Restructured package and included unit test for coin

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='stochasticity',
+    version='0.1.0',
+    author='Jeff White',
+    author_email='thisguy@thejeffwhite.com',
+    packages=['coin',],
+    url='https://github.com/jeffwhite-619/stochasticity'
+    license='LICENCE',
+    description='Modules to generate various sorts of randomness, starting with a simple coin flip.',
+    long_description=open('README.txt').read(),
+    install_requires="random"
+)
