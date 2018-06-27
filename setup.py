a8177fba0836b013682f9f6b70d0d387b956d15e
Jeff White <thejeffwhite@gmail.com>
2018-06-27 11:27:15 -0700

    Removed install_requires parameter

--- setup.py
+++ setup.py
@@ -10,5 +10,4 @@ setup(
     license='LICENCE',
     description='Modules to generate various sorts of randomness, starting with a simple coin flip.',
     long_description=open('README.txt').read(),
-    install_requires="random",
 )
